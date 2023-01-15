2cb74ceafeaf1082f7996321559eaaac9c6e0923
Jens de Waard <jcdewaard@protonmail.com>
2023-01-15 17:10:01 +0100

    work

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,21 @@
 module Main (main) where
 
-import qualified Challenges.Y2022.Day15 as D15
+import qualified Challenges.Y2022.Day16 as D16
+import qualified Challenges.Y2022.Day16 as D16
+import Data.Graph.Inductive (labfilter, prettyPrint)
+import Challenges.Y2022.Day16 (rmNode)
 
 main :: IO ()
 main = do
     contents <- readFile "data/2022/01.txt"
-    let input = D15.parse contents
-    print $ D15.solveA input
-    print $ D15.solveB input
+    let input = D16.parse contents
+    let g = D16.toGraph D16.world
+    let n = 70070
+    let g' = rmNode g n
+    let g'' = rmNode g' 66066
+    prettyPrint g
+    putStrLn "------"
+    prettyPrint $ g'
+    putStrLn "------"
+    prettyPrint $ g''
+    -- print $ D16.solveB input
