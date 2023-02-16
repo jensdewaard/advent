a5f1ad1b07788caa2d2e5dd7e9b359600fd610ff
Jens de Waard <jcdewaard@protonmail.com>
2023-02-16 13:39:50 +0100

    Work on 2022-17

--- app/Main.hs
+++ app/Main.hs
@@ -1,30 +1,7 @@
 module Main (main) where
 
-import qualified Challenges.Y2022.Day16 as D16
-import Data.Graph.Inductive (labfilter, prettyPrint)
-import Challenges.Y2022.Day16 (rmNode, toMatrix, solveA)
-import FloydWarshall (floydwarshall)
+import Challenges.Y2022.Day17 (minmax, solveA, input)
 
 main :: IO ()
 main = do
-    contents <- readFile "data/2022/01.txt"
-    let input = D16.parse contents
-    let g = D16.toGraph D16.world
-    let n = 70070
-    let g' = rmNode g n
-    let g'' = rmNode g' 71071
-    let g''' = rmNode g'' 73073
-    prettyPrint g
-    -- putStrLn "------"
-    -- prettyPrint $ g'
-    putStrLn "------"
-    prettyPrint $ g'''
-    -- print $ D16.solveB input
-    let dm = toMatrix g'''
-    putStrLn "------"
-    print dm
-    let wfi = floydwarshall dm
-    putStrLn "------"
-    print wfi
-    putStrLn "------"
-    print $ D16.solveA D16.world
+    print $ solveA $ input
---
+++ src/Challenges/Y2022/Day17.hs
@@ -0,0 +1,90 @@
+{-# LANGUAGE BinaryLiterals #-}
+module Challenges.Y2022.Day17 (minmax, solveA, input) where
+import Data.Maybe (fromJust)
+import Data.Foldable ( Foldable(toList) )
+import Shared (Coord)
+
+data Dir = L | R | D deriving (Eq, Show)
+type Surface = (Int, Int, Int, Int, Int, Int, Int)
+
+--type Rock = S.Set Coord
+data World = World {
+    surface :: Surface,
+    fallingRock :: Maybe Rock,
+    stackHeight :: Int,
+    rockIndex :: Int,
+    jetIndex :: Int
+}
+
+world :: World
+world = mkWorld $ replicate 7 0
+
+mkWorld :: [Int] -> World
+mkWorld s = World {surface=s, stackHeight=0, fallingRock = Nothing, rockIndex = 0, jetIndex=0}
+
+input :: [Dir]
+input = [R,R,R,L,L,R,L,R,R,L,L,L,R,R,L,R,R,R,L,L,L,R,R,R,L,L,L,R,L,L,L,R,R,L,R,R,L,L,R,R]
+
+wouldCollide :: Rock -> Surface -> Bool
+wouldCollide ((x,y),SquareRock) w = true
+
+move :: Dir -> Rock -> Rock
+move L ((x, y), s) = ((x-1, y), s)
+move R ((x, y), s) = ((x+1, y), s)
+move D ((x, y), s) = ((x, y - 1), s)
+
+minmax :: (Int, Int)
+minmax = (l, t) where
+    is = scanl step 0 input
+    l = minimum is
+    t = maximum is
+    step :: Int -> Dir -> Int
+    step i L = i - 1
+    step i R = i + 1
+    step i D = i
+
+solveA :: [Dir] -> Int
+solveA ds = stackHeight $ (iterate (sim (cycle rocks) (cycle ds)) world !! 2022)
+
+sim :: [Int -> Rock] -> [Dir] -> World -> World
+sim [] _ _ = error "no rocks given"
+sim _ [] _ = error "no jet streams"
+sim rs _ w@World{fallingRock=Nothing} = w{fallingRock=r, rockIndex=(rockIndex w) + 1 } where
+    r = Just $ rs !! (rockIndex w) $ (stackHeight w + 3)
+sim _ ds w = w{fallingRock = mr, jetIndex=(jetIndex w) + 1, surface=stack', stackHeight = sh} where
+        r = fromJust $ fallingRock w
+        dir = ds !! (jetIndex w)
+        r' = move dir r
+        collisionLR = wouldCollide r' w
+        r'' = if collisionLR then r else r'
+        r''' = move D r''
+        collisionD = wouldCollide r''' w
+        mr = if collisionD then Nothing else Just r'''
+        stack' = if collisionD then (surface w) `S.union` r else (surface w)
+        sh = maximum $ Prelude.map snd (toList stack')
+
+--- Rocks
+-- flatRock :: Int -> Rock
+-- flatRock y = S.fromList [(-1, y), (0,y), (1,y), (2,y)]
+
+-- plusRock :: Int -> Rock
+-- plusRock y = S.fromList [(0, y+2), (-1, y+1), (0, y+1), (1, y+1), (0, y)]
+
+-- elRock :: Int -> Rock
+-- elRock y = S.fromList [(-1, y), (0, y), (1, y), (1, y+1), (1, y+2)]
+
+-- thinRock :: Int -> Rock
+-- thinRock y = S.fromList [(-1, y), (-1, y+1), (-1, y+2), (-1, y+3)]
+
+-- squareRock :: Int -> Rock
+-- squareRock y = S.fromList [(-1, y), (-1, y+1), (0, y), (0, y+1)]
+-- rocks :: [Int -> Rock]
+-- rocks = [flatRock, plusRock, elRock, thinRock, squareRock]
+
+type Rock = (Coord, RockShape)
+data RockShape = FlatRock | PlusRock | ElRock | ThinRock | SquareRock
+
+
+normalizeSurface :: [Int] -> ([Int], Int)
+normalizeSurface is = (map (-m) is, m)
+    where m = minimum is
