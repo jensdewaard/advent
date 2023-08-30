23b3bf70e80b518d12c69b753c641627eb33b83c
Jens de Waard <jcdewaard@protonmail.com>
2023-08-30 10:06:15 +0200

    WIP: try to make 2015-6 use map

--- src/Challenges/Y2015/Day06.hs
+++ src/Challenges/Y2015/Day06.hs
@@ -1,37 +1,35 @@
 module Challenges.Y2015.Day06 (parseInput, solutionA, solutionB) where
 import qualified Data.Set as S
+import Data.Map (Map)
+import qualified Data.Map as Map
 import Shared (Coord, solve, rectFromTo)
 import Text.ParserCombinators.Parsec
 
 -- 598216 too high
 
+type MappedToInt a = Map a Int
+
 solutionA :: String -> String
-solutionA input = solve input parseInput (show . length . foldSequence . runSequence)
+solutionA input = solve input parseInput (show . sum . foldSequence . runSequence)
 solutionB :: String -> String
 solutionB _ = "0"
 
-foldSequence :: [(LightAction, S.Set Coord)] -> S.Set Coord
-foldSequence as = foldl doAction S.empty as
+foldSequence :: [(LightAction, MappedToInt Coord)] -> MappedToInt Coord
+foldSequence as = foldl doAction Map.empty as
 
-runSequence :: [(LightAction, Coord, Coord)] -> [(LightAction, S.Set Coord)]
+runSequence :: [(LightAction, Coord, Coord)] -> [(LightAction, MappedToInt Coord)]
 runSequence as = map sndExpand as
 
-sndExpand :: (LightAction, Coord, Coord) -> (LightAction, S.Set Coord)
-sndExpand (a, c1, c2) = (a, S.fromList $ rectFromTo c1 c2)
+sndExpand :: (LightAction, Coord, Coord) -> (LightAction, MappedToInt Coord)
+sndExpand (a, c1, c2) = (a, Map.fromList $ map pairOne (rectFromTo c1 c2)) where
+    pairOne x = (x, 1)
 
 data LightAction = Toggle | On | Off
 
-doAction :: S.Set Coord -> (LightAction, S.Set Coord) -> S.Set Coord
-doAction cs (Toggle, cs') = foldl toggle cs cs' 
-doAction cs (On, cs') = foldl on cs cs' 
-doAction cs (Off, cs') = foldl off cs cs' 
-
-toggle :: S.Set Coord -> Coord -> S.Set Coord
-toggle cs c = if S.member c cs then off cs c else on cs c
-on :: S.Set Coord -> Coord -> S.Set Coord
-on cs c = S.insert c cs
-off :: S.Set Coord -> Coord -> S.Set Coord
-off cs c = S.delete c cs
+doAction :: MappedToInt Coord -> (LightAction, MappedToInt Coord) -> MappedToInt Coord
+doAction cs (Toggle, cs') = Map.unionWith (\a -> \_ -> 1 - a) cs cs'
+doAction cs (On, cs') = Map.unionWith (\a -> \_ -> 1) cs cs'
+doAction cs (Off, cs') = Map.unionWith (\a -> \_ -> 0) cs cs'
 
 -- Parsers
 parseInput :: Parser [(LightAction, Coord, Coord)]
