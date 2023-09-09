2c5f4945de6bd517e4d70ec7bba7d2b116dcd743
Jens de Waard <jcdewaard@protonmail.com>
2023-09-09 08:52:54 +0200

    Solve 2015/12

---
+++ src/Challenges/Y2015/Day12.hs
@@ -0,0 +1,42 @@
+module Challenges.Y2015.Day12 (solutionA, solutionB) where
+
+import Data.Aeson (Value (Null, Number, Object, Array, String, Bool), fromJSON)
+import Data.Aeson.Decoding (decode)
+import Data.Aeson.KeyMap (elems)
+import qualified Data.ByteString.Lazy as BS
+import Data.Scientific (Scientific, toBoundedInteger, coefficient)
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.Encoding as TLE
+import Data.Maybe (fromJust)
+    
+solutionA :: String -> String
+solutionA = show . foldValue 0 . decodeJson
+
+decodeJson :: String -> Value
+decodeJson = fromJust . decode . TLE.encodeUtf8 . TL.pack
+
+toUnboundedInteger :: Scientific -> Int
+toUnboundedInteger = fromInteger . coefficient
+
+foldValue :: Int -> Value -> Int
+foldValue n (Number s) = n + toUnboundedInteger s
+foldValue n (Object km) = foldl foldValue n km
+foldValue n (Array es) = foldl foldValue n es
+foldValue n (String _) = n
+foldValue n (Bool _) = n
+foldValue n (Null) = n
+
+foldValueB :: Int -> Value -> Int
+foldValueB n (Object km) = if any isRed $ elems km then n else foldl foldValueB n km
+foldValueB n (Array es) = foldl foldValueB n es
+foldValueB n j = foldValue n j
+
+
+isRed :: Value -> Bool
+isRed (String s) = (show s) == show "red"
+isRed _ = False
+
+solutionB :: String -> String
+solutionB = show . foldValueB 0 . decodeJson
+
+
