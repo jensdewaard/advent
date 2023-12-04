f685fccb44a916756e876c6c9cd86fe7c274a3ee
Jens de Waard <jcdewaard@protonmail.com>
2023-12-04 19:49:49 +0100

    2023-04

--- src/Challenges/Y2023.hs
+++ src/Challenges/Y2023.hs
@@ -3,8 +3,10 @@ module Challenges.Y2023 (getDay) where
 import qualified Challenges.Y2023.Day01 as Day01
 import qualified Challenges.Y2023.Day02 as Day02
 import qualified Challenges.Y2023.Day03 as Day03
+import qualified Challenges.Y2023.Day04 as Day04
 
 getDay :: Int -> (String-> String, String -> String)
 getDay 1 = (Day01.solutionA, Day01.solutionB)
 getDay 2 = (Day02.solutionA, Day02.solutionB)
 getDay 3 = (Day03.solutionA, Day03.solutionB)
+getDay 4 = (Day04.solutionA, Day04.solutionB)
---
+++ src/Challenges/Y2023/Day04.hs
@@ -0,0 +1,47 @@
+module Challenges.Y2023.Day04 where
+import Text.ParserCombinators.Parsec
+
+import Data.List (intersect, union, sortBy)
+import Shared (solve)
+
+solutionA :: String -> String
+solutionA = solve parseInput (sum . map score)
+
+solutionB :: String -> String
+solutionB = solve parseInput (sum . map snd . copies . initCards)
+
+cardId :: Card -> Int
+cardId (i,_,_) = i
+
+initCards :: [Card] -> [(Card, Int)]
+initCards cs = zip cs $ repeat 1
+
+inc :: Int -> Int -> [(Card, Int)] -> [(Card, Int)]
+inc _ 0 cs = cs
+inc m n ((c,k):cs) = (c,k+m) : inc m (n-1) cs
+
+copies :: [(Card, Int)] -> [(Card, Int)]
+copies [] = []
+copies (p@(c@(_,ws,hs),i):cs) = let n = length $ intersect ws hs in
+    p : copies (inc i n cs)
+
+score :: Card -> Int
+score (_, ws, hs) = case length $ intersect ws hs of
+    0 -> 0
+    n -> 2^(n-1)
+
+type Card = (Int, [Int], [Int])
+
+parseInput :: Parser [Card]
+parseInput = sepBy1 parseCard newline
+
+parseCard :: Parser Card
+parseCard = do
+    _ <- string "Card" >> optional spaces
+    n <- many1 digit
+    _ <- string ": " >> optional spaces
+    -- winners <- manyTill (many1 digit) (try $ string " | ")
+    winners <- sepEndBy1 (many1 digit) (many1 $ char ' ')
+    _ <- string "|" >> optional spaces
+    haves <- sepBy (many1 digit) (many1 $ char ' ')
+    return (read n, map read winners, map read haves)
