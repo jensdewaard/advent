128a10bee89a6d2763eb4b250f1254ea979453db
Jens de Waard <jcdewaard@protonmail.com>
2023-12-13 09:50:12 +0100

    uncurry assignSpring

--- src/Challenges/Y2023/Day12.hs
+++ src/Challenges/Y2023/Day12.hs
@@ -6,7 +6,7 @@ import Data.Function (fix)
 import Data.List (intercalate)
 
 solutionA :: String -> String
-solutionA = solve parser (sum . map ( (fix assignSpring)))
+solutionA = solve parser (sum . map ( uncurry (fix assignSpring)))
 solutionB :: String -> String
 -- solutionB = solve parser (sum . map (length . (fix assignSpring) . unfold))
 solutionB = const ""
@@ -16,23 +16,23 @@ type Row = ([Char], [Int])
 unfold :: Row -> Row
 unfold (r,g) = (intercalate "?" $ replicate 5 (r ++ ['?']), concat $ replicate 5 g)
 
-assignSpring :: (([Char], [Int]) -> Int) -> ([Char], [Int]) -> Int
+assignSpring :: ([Char] -> [Int] -> Int) -> [Char] -> [Int] -> Int
 -- if there are still unmatched groups, this is not a valid solution branch
-assignSpring _ ([],b) = if null b then 1 else 0
+assignSpring _ [] b = if null b then 1 else 0
 -- if there are still # but no more groups, this is not a valid solution branch
-assignSpring _ (a,[]) = if '#' `elem` a then 0 else 1
+assignSpring _  a []  = if '#' `elem` a then 0 else 1
 -- check the first character
-assignSpring f ('?':cs, g:gs) =  f ('#':cs, g:gs) + f ('.':cs,g:gs)
-assignSpring f ('.':cs, gs) = f (cs, gs)
-assignSpring f (s@('#':_), g:gs)
+assignSpring f ('?':cs) gs =  f ('#':cs) gs + f ('.':cs) gs
+assignSpring f ('.':cs)  gs = f cs  gs
+assignSpring f s@('#':_)  (g:gs)
     | '.' `elem` take g s = 0 -- illegal string, because there are dots part of the group
     | length s < g = 0        -- illegal string, because it's shorter than the size of the next group 
-    | length s == g = f ("", gs) -- end of string
+    | length s == g = f "" gs -- end of string
     | '#' == head (drop g s) = 0 -- check the next character after the group of g springs
     | otherwise = let
         springs = replicate g '#' ++ ['.']
-        in f (drop (length springs) s, gs)
-assignSpring _ (s,_) = error ("illegal character in string: " ++ s)
+        in f (drop (length springs) s) gs
+assignSpring _  s _ = error ("illegal character in string: " ++ s)
 
 parser :: Parser [Row]
 parser = row `sepEndBy1` newline where
