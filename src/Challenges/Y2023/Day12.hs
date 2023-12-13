a2365010195e4588037281ea3e88c254e12bbfec
Jens de Waard <jcdewaard@protonmail.com>
2023-12-13 09:47:23 +0100

    have assignSpring return Int

--- src/Challenges/Y2023/Day12.hs
+++ src/Challenges/Y2023/Day12.hs
@@ -2,34 +2,37 @@ module Challenges.Y2023.Day12 (solutionA, solutionB) where
 import Text.ParserCombinators.Parsec
 import Shared (solve, prepend)
 import Parsing (int)
+import Data.Function (fix)
+import Data.List (intercalate)
 
 solutionA :: String -> String
-solutionA = solve parser (sum . map (length . assignSpring))
+solutionA = solve parser (sum . map ( (fix assignSpring)))
 solutionB :: String -> String
-solutionB = solve parser (sum . map (length . assignSpring . unfold))
+-- solutionB = solve parser (sum . map (length . (fix assignSpring) . unfold))
+solutionB = const ""
 
 type Row = ([Char], [Int])
 
 unfold :: Row -> Row
-unfold (r,g) = (init $ concat $ replicate 5 (r ++ ['?']), concat $ replicate 5 g)
+unfold (r,g) = (intercalate "?" $ replicate 5 (r ++ ['?']), concat $ replicate 5 g)
 
-assignSpring :: Row -> [Row]
+assignSpring :: (([Char], [Int]) -> Int) -> ([Char], [Int]) -> Int
 -- if there are still unmatched groups, this is not a valid solution branch
-assignSpring ([],b) = [([],[]) | null b]
+assignSpring _ ([],b) = if null b then 1 else 0
 -- if there are still # but no more groups, this is not a valid solution branch
-assignSpring (a,[]) = [(replicate (length a) '.',[]) | '#' `notElem` a]
+assignSpring _ (a,[]) = if '#' `elem` a then 0 else 1
 -- check the first character
-assignSpring ('?':cs, g:gs) =  assignSpring ('#':cs, g:gs) ++ assignSpring ('.':cs,g:gs)
-assignSpring ('.':cs, gs) = map (prepend (['.'],[])) $ assignSpring (cs, gs)
-assignSpring (s@('#':_), g:gs)
-    | '.' `elem` take g s = [] -- illegal string, because there are dots part of the group
-    | length s < g = []        -- illegal string, because it's shorter than the size of the next group 
-    | length s == g = map (prepend (replicate g '#', [g])) $ assignSpring ("", gs) -- end of string
-    | '#' == head (drop g s) = [] -- check the next character after the group of g springs
+assignSpring f ('?':cs, g:gs) =  f ('#':cs, g:gs) + f ('.':cs,g:gs)
+assignSpring f ('.':cs, gs) = f (cs, gs)
+assignSpring f (s@('#':_), g:gs)
+    | '.' `elem` take g s = 0 -- illegal string, because there are dots part of the group
+    | length s < g = 0        -- illegal string, because it's shorter than the size of the next group 
+    | length s == g = f ("", gs) -- end of string
+    | '#' == head (drop g s) = 0 -- check the next character after the group of g springs
     | otherwise = let
         springs = replicate g '#' ++ ['.']
-        in map (prepend (springs, [g])) $ assignSpring (drop (length springs) s, gs)
-assignSpring _ = error "illegal character in string"
+        in f (drop (length springs) s, gs)
+assignSpring _ (s,_) = error ("illegal character in string: " ++ s)
 
 parser :: Parser [Row]
 parser = row `sepEndBy1` newline where
