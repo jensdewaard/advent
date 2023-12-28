0ea954f136631fa9918fadad7e0235c249680d8f
Jens de Waard <jcdewaard@protonmail.com>
2023-12-28 15:48:26 +0100

    redo 2022-01/02

--- src/Challenges/Y2022/Day01.hs
+++ src/Challenges/Y2022/Day01.hs
@@ -1,8 +1,14 @@
-module Challenges.Y2022.Day01 where
+module Challenges.Y2022.Day01 (solutionA, solutionB) where
 
 import Data.List ( sortBy )
+import Common.Prelude
 import Common.Parsing (int)
 import Text.ParserCombinators.Parsec
+
+solutionA :: String -> String
+solutionA = parseFile ==> partA
+solutionB :: String -> String
+solutionB = parseFile ==> partB
 
 partA :: [Elf] -> Int
 partA elves = maximum $ sortBy (flip compare) (map sum elves)
@@ -10,13 +16,8 @@ partA elves = maximum $ sortBy (flip compare) (map sum elves)
 partB :: [Elf] -> Int
 partB elves = sum . take 3 $ sortBy (flip compare) (map sum elves)
 
-parseFile :: String -> Either ParseError [Elf]
-parseFile = parse file "could not parse file" where
-    file = elf `endBy1` newline
+parseFile :: Parser [Elf]
+parseFile = elf `sepEndBy1` newline where
     elf = int `endBy1` newline
 
 type Elf = [Int]
-
-maybeFromEither :: Either a b -> Maybe b
-maybeFromEither (Right x) = Just x
-maybeFromEither (Left _) = Nothing
--- src/Challenges/Y2022/Day02.hs
+++ src/Challenges/Y2022/Day02.hs
@@ -1,66 +1,67 @@
-#!/usr/bin/env nix-shell
-#!nix-shell --pure -i runghc -p "haskellPackages.ghcWithPackages (pkgs: [ pkgs.turtle ])"
+module Challenges.Y2022.Day02 (solutionA, solutionB) where
 
-module Challenges.Y2022.Day02 where
+import Common.Prelude
+import Text.ParserCombinators.Parsec
 
-import qualified Data.Text as T
+solutionA :: String -> String
+solutionA = parser game  ==> sum . map score
+solutionB :: String -> String
+solutionB = parser gameB ==> sum . map score'
 
-main :: IO ()
-main = do
-    contents <- readFile "inputs/2.txt"
-    let lines = T.splitOn (T.pack "\n") (T.pack contents)
-    let rounds = map (readRound . T.unpack) lines
-    let rounds' = map (readRound' . T.unpack) lines
-    print ("Strategy score: " ++ show (sum (map score rounds)))
-    print ("Stategy score with results: " ++ show (sum (map score' rounds')))
-    
+parser :: Parser a -> Parser [a]
+parser p = p `sepEndBy` newline
+
+game :: Parser Round
+game = do
+    l <- move
+    _ <- space
+    r <- move
+    return (l,r)
+
+gameB :: Parser Round'
+gameB = do
+    l <- move
+    _ <- space
+    r <- result
+    return (l,r)
+
+move :: Parser Move
+move = (char 'A' >> return Rock)
+    <|> (char 'X' >> return Rock)
+    <|> (char 'B' >> return Paper)
+    <|> (char 'Y' >> return Paper)
+    <|> (char 'C' >> return Scissors)
+    <|> (char 'Z' >> return Scissors)
+
+result :: Parser Result
+result = (char 'X' >> return Lose)
+    <|> (char 'Y' >> return Draw)
+    <|> (char 'Z' >> return Win)
+
 data Move = Rock | Paper | Scissors
 data Result = Win | Lose | Draw
-
-readMove :: Char -> Move
-readMove 'A' = Rock
-readMove 'X' = Rock
-readMove 'B' = Paper
-readMove 'Y' = Paper
-readMove 'C' = Scissors
-readMove 'Z' = Scissors
-
-readResult :: Char -> Result
-readResult 'X' = Lose
-readResult 'Y' = Draw
-readResult 'Z' = Win
 
 type Round = (Move, Move)
 type Round' = (Move, Result)
 
-readRound :: String -> Maybe Round
-readRound [l, ' ', r] = Just (readMove l, readMove r)
-readRound "" = Nothing
+score :: Round -> Int
+score (Rock, Rock) = 1 + 3
+score (Rock, Paper) = 2 + 6
+score (Rock, Scissors) = 3 + 0
+score (Paper, Rock) = 1 + 0
+score (Paper, Paper) = 2 + 3
+score (Paper, Scissors) = 3 + 6
+score (Scissors, Rock) = 1 + 6
+score (Scissors, Paper) = 2 + 0
+score (Scissors, Scissors) = 3 + 3
 
-readRound' :: String -> Maybe Round'
-readRound' [l, ' ', r] = Just(readMove l, readResult r)
-readRound' "" = Nothing
-
-score :: Maybe Round -> Int
-score (Just (Rock, Rock)) = 1 + 3
-score (Just (Rock, Paper)) = 2 + 6
-score (Just (Rock, Scissors)) = 3 + 0
-score (Just (Paper, Rock)) = 1 + 0
-score (Just (Paper, Paper)) = 2 + 3
-score (Just (Paper, Scissors)) = 3 + 6
-score (Just (Scissors, Rock)) = 1 + 6
-score (Just (Scissors, Paper)) = 2 + 0
-score (Just (Scissors, Scissors)) = 3 + 3
-score Nothing = 0
-
-score' :: Maybe Round' -> Int
-score' (Just (Rock, Draw)) = 1 + 3
-score' (Just (Rock, Win)) = 2 + 6
-score' (Just (Rock, Lose)) = 3 + 0
-score' (Just (Paper, Lose)) = 1 + 0
-score' (Just (Paper, Draw)) = 2 + 3
-score' (Just (Paper, Win)) = 3 + 6
-score' (Just (Scissors, Win)) = 1 + 6
-score' (Just (Scissors, Lose)) = 2 + 0
-score' (Just (Scissors, Draw)) = 3 + 3
-score' Nothing = 0
+score' :: Round' -> Int
+score' (Rock, Draw) = 1 + 3
+score' (Rock, Win) = 2 + 6
+score' (Rock, Lose) = 3 + 0
+score' (Paper, Lose) = 1 + 0
+score' (Paper, Draw) = 2 + 3
+score' (Paper, Win) = 3 + 6
+score' (Scissors, Win) = 1 + 6
+score' (Scissors, Lose) = 2 + 0
+score' (Scissors, Draw) = 3 + 3
