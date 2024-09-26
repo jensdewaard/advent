2cd1242b1121b63eedf2800faa8b9d0a9363aba9
Jens de Waard <jcdewaard@protonmail.com>
2024-09-26 10:36:51 +0200

    feat(2017): Solve day 02

--- src/Challenges/Y2017/Day02.hs
+++ src/Challenges/Y2017/Day02.hs
@@ -1,11 +1,36 @@
 module Challenges.Y2017.Day02 (solutionA, solutionB) where
+
+import Common.Coord
+import Common.Parsing (grid, int)
 import Common.Prelude
+import Data.Map (Map)
+import qualified Data.Map as Map
 import Text.ParserCombinators.Parsec
 
 solutionA :: String -> String
-solutionA = solve parser (const "")
-solutionB :: String -> String
-solutionB = solve parser (const "")
+solutionA = solve parser (sum . map diff . rows)
 
-parser :: Parser ()
-parser = undefined
+solutionB :: String -> String
+solutionB = solve parser (sum . concatMap divisors . rows)
+
+parser :: Parser (Map Coord Int)
+parser =
+  grid
+    ( do
+        i <- int
+        _ <- optional spaces
+        return i
+    )
+
+rows :: Map Coord Int -> [[Int]]
+rows m =
+  let maxY = maximum $ map (\((_, y), _) -> y) ls
+      ls = Map.assocs m
+      f r ((_, y), _) = y == r
+   in map (\r -> map snd $ filter (f r) ls) [1 .. maxY]
+
+diff :: [Int] -> Int
+diff ns = maximum ns - minimum ns
+
+divisors :: [Int] -> [Int]
+divisors ns = [c | a <- ns, b <- ns, let c = a `div` b, a `mod` b == 0, a /= b]
