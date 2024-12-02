82a3a0db920df2d4942eb632e72661882d74631e
Jens de Waard <jcdewaard@protonmail.com>
2024-12-02 08:22:22 +0100

    refactor(2016): Remove unused functions

--- src/Challenges/Y2016/Day14.hs
+++ src/Challenges/Y2016/Day14.hs
@@ -1,9 +1,6 @@
 module Challenges.Y2016.Day14 (solutionA, solutionB) where
 import Common.Prelude
 import Text.ParserCombinators.Parsec
-import Data.Digest.Pure.MD5 (md5)
-import Data.String (fromString)
-import Data.MemoCombinators (integral)
 import qualified    Data.ByteString.Char8 as Bytes
 import Crypto.Hash
 import              Data.ByteArray.Encoding
@@ -12,7 +9,6 @@ solutionA :: String -> String
 solutionA = solve parser (\salt -> iterate (key 0 salt) 0 !! 64)
 solutionB :: String -> String
 solutionB = solve parser (\salt -> iterate (key 2016 salt) 0 !! 64)
---solutionB = solve parser (const "")
 
 parser :: Parser String
 parser = many1 alphaNum
@@ -20,13 +16,6 @@ parser = many1 alphaNum
 key :: Int -> String -> Int -> Int
 key stretch salt n = let k' salt' n' = if isKey stretch salt' n' == Just True then n' else k' salt' (n'+1) in
     k' salt (n+1)
-
-hashStretchedM :: Int -> String -> Int -> String
-hashStretchedM n salt = integral (hashStretched n salt)
-hashStretched :: Int -> String -> Int -> String
-hashStretched n salt index = iterate hash (salt <> show index) !! n where
-    hash :: String -> String
-    hash = show . md5 . fromString
 
 containsThree :: String -> Maybe Char
 containsThree [] = Nothing
