98ee6236e0b226d078009ddc69ead9fad518e272
Jens de Waard <jcdewaard@protonmail.com>
2024-12-07 11:39:52 +0100

    Solve 2024-07

--- src/Challenges/Y2024/Day07.hs
+++ src/Challenges/Y2024/Day07.hs
@@ -1,12 +1,34 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 module Challenges.Y2024.Day07 (solutionA, solutionB) where
 import Common.Prelude (solve)
-import Text.ParserCombinators.Parsec (Parser)
-import Control.Arrow ((>>>), (&&&))
+import Text.ParserCombinators.Parsec (Parser, newline, string, many1, sepEndBy1, space, sepBy1, char)
+import Control.Arrow ((>>>))
+import Data.Maybe (mapMaybe, isJust)
+import Common.Parsing (int)
+import Control.Applicative ((<|>), Alternative (some), asum)
 
 solutionA :: String -> String
-solutionA = solve parser (const "")
+solutionA = solve parser (mapMaybe (canCalibrate [(+),(*)]) >>> sum)
 solutionB :: String -> String
-solutionB = solve parser (const "")
+solutionB = solve parser (mapMaybe (canCalibrate [(+),(*), concatOp]) >>> sum)
 
-parser :: Parser ()
-parser = undefined
+data Equation = Eqn Int [Int] deriving (Eq, Show)
+
+type Operator = Int -> Int -> Int
+canCalibrate :: [Operator] -> Equation -> Maybe Int
+canCalibrate _ (Eqn _ []) = Nothing
+canCalibrate _ (Eqn total [a]) = if a == total then Just total else Nothing
+canCalibrate ops (Eqn total (v:w:vs)) = asum (map (\op -> canCalibrate ops (Eqn total (op v w:vs))) ops)
+
+concatOp :: Int -> Int -> Int
+concatOp a b = let
+    s = show a
+    t = show b in read (s <> t)
+
+parser :: Parser [Equation]
+parser = equation `sepEndBy1` newline where
+    equation = do
+        total <- int
+        _ <- string ": "
+        ns <- int `sepBy1` char ' '
+        return (Eqn total ns)
