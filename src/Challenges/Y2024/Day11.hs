75915c8187b35de8d7ddf1df7ff946ada53f3fbf
Jens de Waard <jcdewaard@protonmail.com>
2024-12-11 07:02:15 +0100

    Solve 2024-11a

--- src/Challenges/Y2024/Day11.hs
+++ src/Challenges/Y2024/Day11.hs
@@ -1,12 +1,33 @@
 module Challenges.Y2024.Day11 (solutionA, solutionB) where
 import Common.Prelude (solve)
-import Text.ParserCombinators.Parsec (Parser)
+import Text.ParserCombinators.Parsec (Parser, many1, spaces, sepEndBy1)
 import Control.Arrow ((>>>), (&&&))
+import Data.Maybe (isJust, fromJust)
+import Common.Parsing (int)
+import qualified Common.LinkedList as LL
+import Common.LinkedList (LinkedList(..))
 
 solutionA :: String -> String
-solutionA = solve parser (const "")
+solutionA = solve parser (iterate blink >>> (!!25) >>> LL.length )
 solutionB :: String -> String
-solutionB = solve parser (const "")
+solutionB = solve parser (iterate blink >>> (!!0) >>> LL.length )
 
-parser :: Parser ()
-parser = undefined
+blink :: Stone -> Stone
+blink ListEnd = ListEnd
+blink (Link txt ns)
+    | txt == "0" = Link "1" (blink ns)
+    | even (length txt)  = let
+        h = length txt `div` 2
+        e1 :: Int
+        e1 = read $ take h txt
+        e2 :: Int
+        e2 = read $ drop h txt
+        in Link (show e1) (Link (show e2) (blink ns))
+    | otherwise = Link (show (2024 * read txt)) (blink ns)
+
+type Stone = LinkedList String
+
+parser :: Parser Stone
+parser = do
+    ns <- int `sepEndBy1` spaces
+    return $ LL.fromList $ map show ns
---
+++ src/Common/LinkedList.hs
@@ -0,0 +1,36 @@
+module Common.LinkedList where
+
+import Prelude hiding (length)
+
+data LinkedList a = Link
+    {   value :: a
+    ,   next :: LinkedList a
+    } | ListEnd
+
+cons :: LinkedList a -> LinkedList a -> LinkedList a
+cons ListEnd m = m
+cons (Link x n) m = Link x (cons n m)
+
+instance Functor LinkedList where
+    fmap :: (a -> b) -> LinkedList a -> LinkedList b
+    fmap f (Link a n) = Link (f a) (f <$> n)
+    fmap _ ListEnd = ListEnd
+
+instance Applicative LinkedList where
+  pure :: a -> LinkedList a
+  pure x = Link x ListEnd
+  (<*>) :: LinkedList (a -> b) -> LinkedList a -> LinkedList b
+  (<*>) (Link f fn) (Link a an) = Link (f a) (fn <*> an)
+  (<*>) _ _ = ListEnd
+
+fromList :: [a] -> LinkedList a
+fromList = foldr Link ListEnd
+
+instance Foldable LinkedList where
+  foldr :: (a -> b -> b) -> b -> LinkedList a -> b
+  foldr f acc (Link a n) = f a (foldr f acc n)
+  foldr _ acc ListEnd = acc
+
+length :: LinkedList a -> Int
+length ListEnd = 0
+length (Link _ n) = 1 + length n
