7eb6768458bff66f7a6667475165e6167fc5e261
Jens de Waard <jcdewaard@protonmail.com>
2025-01-21 12:08:16 +0100

    work on 2025-20

--- src/Challenges/Y2024/Day20.hs
+++ src/Challenges/Y2024/Day20.hs
@@ -3,76 +3,53 @@
 module Challenges.Y2024.Day20 (solutionA, solutionB) where
 
 import Common.Coord (Coord, cardinal, dist)
+import Common.List (count, sublist, sumWith)
 import Common.Parsing (grid)
 import Common.Prelude (solve)
-import Common.Search (Dijkstra (..), liftCost, searchFor)
-import Control.Arrow ((&&&), (>>>))
-import Data.Map (Map)
-import Data.List (singleton)
+import Data.Array (Array, array, (!))
 import qualified Data.Map as M
-import Data.Ord (comparing)
 import Data.Set (Set)
 import qualified Data.Set as S
 import Text.ParserCombinators.Parsec (Parser, alphaNum, char, (<|>))
-import Data.Maybe (fromJust)
-import Common.List (occur, sumWith)
 
 solutionA :: String -> String
-solutionA = solve parser (singleton >>> searchFor (\s -> position s == destination s)
-    >>> fromJust
-    >>> fst 
-    >>> pathTaken
-    >>> shortcuts
-    >>> occur
-    >>> filter ((>=100) . fst)
-    >>> sumWith snd
-    )
+solutionA = solve parser (cheats goal 2) where goal = 100
 
 solutionB :: String -> String
 solutionB = solve parser (const "")
 
-data State = State
-  { position :: Coord,
-    pathTaken :: [Coord],
-    destination :: Coord,
-    walls :: Set Coord
-  }
-  deriving (Show)
+type Track = Array Int Coord
 
-instance Eq State where
-  (==) s t = position s == position t
+data Maze = Maze Coord Coord (Set Coord) deriving (Show)
 
-instance Ord State where
-  compare = comparing position
+findTrack :: Maze -> [Coord]
+findTrack = go []
+  where
+    go visited (Maze start end walls)
+      | start == end = []
+      | otherwise =
+          let c = head $ filter (\x -> x `notElem` walls && x `notElem` visited) $ cardinal start
+           in c : go (c : visited) (Maze c end walls)
 
-instance Dijkstra State where
-  type DijkstraRepr State = Coord
-  type DijkstraCost State = Int
-  represent = position
-  adjacency = liftCost findPath
-  estimate = const 1
+validCheat :: Int -> Int -> (Int, Coord) -> (Int, Coord) -> Bool
+validCheat maxCheatDistance targetedSaving start end =
+  let d = dist (snd start) (snd end)
+   in d <= maxCheatDistance && d <= (fst end - fst start - targetedSaving)
 
-findPath :: State -> [State]
-findPath s =
-  [ s {position = p, pathTaken = p : pathTaken s}
-    | p <- cardinal (position s),
-      p `notElem` walls s
-  ]
+cheats :: Int -> Int -> Track -> Int
+cheats goal cheat track = sumWith f trackIndices
+  where
+    trackIndices = [1 .. length track]
+    f start =
+      count
+        (\end -> validCheat goal cheat (start, track ! start) (end, track ! end))
+        (sublist (start + 1) (start + goal) trackIndices)
 
-shortcuts :: [Coord] -> [Int]
-shortcuts cs = [ timeSaved
-    | i1 <- [0..length cs - 1]
-    , i2 <- [i1 .. length cs - 1]
-    , i1 /= i2
-    , dist (cs !! i1) (cs !! i2) == 2
-    , let timeSaved = i2 - (i1 + 2)
-    , timeSaved /= 2
-    ]
-
-parser :: Parser State
-parser = do 
-    g <- grid (alphaNum <|> char '#' <|> char '.')
-    let ws = S.fromList $ map fst $ M.toList $ M.filter (=='#') g
-    let s = fst $ head $ M.toList $ M.filter (=='S') g
-    let e = fst $ head $ M.toList $ M.filter (=='E') g
-    return $ State s [] e ws
+parser :: Parser Track
+parser = do
+  g <- grid (alphaNum <|> char '#' <|> char '.')
+  let ws = S.fromList $ map fst $ M.toList $ M.filter (== '#') g
+  let s = fst $ head $ M.toList $ M.filter (== 'S') g
+  let e = fst $ head $ M.toList $ M.filter (== 'E') g
+  let t = findTrack $ Maze s e ws
+  return $ array (1, length t) $ zip [1 ..] t
--- src/Common/List.hs
+++ src/Common/List.hs
@@ -1,35 +1,39 @@
 {-# LANGUAGE GADTs #-}
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
-module Common.List (
-  chunksOf,
-  count,
-  deleteAll,
-  endsWith,
-  findCycle,
-  longest,
-  moveElem,
-  occur,
-  prepend,
-  replace,
-  rotate,
-  rotateR,
-  splitOn,
-  sumWith,
-  swapElems,
-  takeUntil,
-  uninterleave,
-  aggregateOn,
-  firstWhere,
-  firstIndex,
-  monotone, differences,
-  subsets,
-  none,
-  sorted
-  ) where
+
+module Common.List
+  ( chunksOf,
+    count,
+    deleteAll,
+    endsWith,
+    findCycle,
+    longest,
+    moveElem,
+    occur,
+    prepend,
+    replace,
+    rotate,
+    rotateR,
+    splitOn,
+    sumWith,
+    swapElems,
+    takeUntil,
+    uninterleave,
+    aggregateOn,
+    firstWhere,
+    firstIndex,
+    monotone,
+    differences,
+    subsets,
+    none,
+    sorted,
+    sublist,
+  )
+where
 
 import Common.Prelude
-import Data.List (delete, isPrefixOf)
 import Data.Bifunctor (first, second)
+import Data.List (delete, isPrefixOf)
 
 chunksOf :: Int -> [a] -> [[a]]
 chunksOf _ [] = []
@@ -39,28 +43,31 @@ chunksOf n rs = take n rs : chunksOf n (drop n rs)
 --   or the source list is empty.
 takeUntil :: Predicate [a] -> [a] -> [a]
 takeUntil _ [] = []
-takeUntil p as = go p [] as where
-  go :: ([a] -> Bool) -> [a] -> [a] -> [a]
-  go _ prefix [] = prefix
-  go prd prefix (x:xs) = if prd prefix
-    then prefix
-    else go prd (prefix ++ [x]) xs
+takeUntil p as = go p [] as
+  where
+    go :: ([a] -> Bool) -> [a] -> [a] -> [a]
+    go _ prefix [] = prefix
+    go prd prefix (x : xs) =
+      if prd prefix
+        then prefix
+        else go prd (prefix ++ [x]) xs
 
-endsWith :: Eq a => a -> [a] -> Bool
+endsWith :: (Eq a) => a -> [a] -> Bool
 endsWith a as = last as == a
 
-sumWith :: Num b => (a -> b) -> [a] -> b
-sumWith f =  sum . map f
+sumWith :: (Num b) => (a -> b) -> [a] -> b
+sumWith f = sum . map f
 
 -- | Count the number of elements in a list that satisfy a predicate.
-count :: Foldable f => Predicate a -> f a -> Int
+count :: (Foldable f) => Predicate a -> f a -> Int
 count predicate = foldr (\e n -> if predicate e then 1 + n else n) 0
 
 -- | The number of occurences of an element in a list.
-occur :: Eq a => [a] -> [(a, Int)]
+occur :: (Eq a) => [a] -> [(a, Int)]
 occur [] = []
-occur (a:as) = let n = 1 + length (filter (==a) as) in
-  (a, n) : occur (filter (a /=) as)
+occur (a : as) =
+  let n = 1 + length (filter (== a) as)
+   in (a, n) : occur (filter (a /=) as)
 
 -- | Return the longest list. Returns the first if there are multiple lists of the same length.
 longest :: [[a]] -> [a]
@@ -69,153 +76,163 @@ longest ls = let l = maximum $ map length ls in head $ filter (\l' -> length l' 
 
 firstWhere :: (a -> Bool) -> [a] -> Maybe a
 firstWhere _ [] = Nothing
-firstWhere predicate (a:as) = if predicate a then Just a else firstWhere predicate as
+firstWhere predicate (a : as) = if predicate a then Just a else firstWhere predicate as
 
 firstIndex :: (a -> Bool) -> [a] -> Maybe Int
 firstIndex _ [] = Nothing
-firstIndex predicate as = go 0 (length as - 1) where
-  go ptr1 ptr2
-    | ptr2 < ptr1 = error "bounds have crossed"
-    | ptr1 == ptr2 = if predicate (as !! ptr1) then Just ptr1 else Nothing
-    | otherwise =
-      if predicate (as !! halfway)
-        then go ptr1 nextPoint
-        else go nextPoint ptr2
-        where halfway = ptr1 + ((ptr2 - ptr1) `div` 2)
-              nextPoint
-                | halfway == ptr1 = ptr1 + 1
-                | halfway == ptr2 = ptr2 - 1
-                | otherwise = halfway
+firstIndex predicate as = go 0 (length as - 1)
+  where
+    go ptr1 ptr2
+      | ptr2 < ptr1 = error "bounds have crossed"
+      | ptr1 == ptr2 = if predicate (as !! ptr1) then Just ptr1 else Nothing
+      | otherwise =
+          if predicate (as !! halfway)
+            then go ptr1 nextPoint
+            else go nextPoint ptr2
+      where
+        halfway = ptr1 + ((ptr2 - ptr1) `div` 2)
+        nextPoint
+          | halfway == ptr1 = ptr1 + 1
+          | halfway == ptr2 = ptr2 - 1
+          | otherwise = halfway
 
-prepend :: ([a],[b]) -> ([a],[b]) -> ([a],[b])
-prepend (a,b) (as,bs) = (a ++ as, b ++ bs)
+prepend :: ([a], [b]) -> ([a], [b]) -> ([a], [b])
+prepend (a, b) (as, bs) = (a ++ as, b ++ bs)
 
--- | Finds a cycle on a list. 
-findCycle :: Eq a => [a] -> ([a],[a])
+-- | Finds a cycle on a list.
+findCycle :: (Eq a) => [a] -> ([a], [a])
 findCycle xxs = fCycle xxs xxs
-  where fCycle (x:xs) (_:y:ys)
-         | x == y              = fStart xxs xs
-         | otherwise           = fCycle xs ys
-        fCycle _      _        = (xxs,[]) -- not cyclic
-        fStart (x:xs) (y:ys)
-          | x == y              = ([], x:fLength x xs)
-          | otherwise           = let (as,bs) = fStart xs ys in (x:as,bs)
-        fStart _ _              = error "fStart called on empty lists"
-        fLength x (y:ys)
-         | x == y              = []
-         | otherwise           = y:fLength x ys
-        fLength _ _            = error "fLength called on empty list"
+  where
+    fCycle (x : xs) (_ : y : ys)
+      | x == y = fStart xxs xs
+      | otherwise = fCycle xs ys
+    fCycle _ _ = (xxs, []) -- not cyclic
+    fStart (x : xs) (y : ys)
+      | x == y = ([], x : fLength x xs)
+      | otherwise = let (as, bs) = fStart xs ys in (x : as, bs)
+    fStart _ _ = error "fStart called on empty lists"
+    fLength x (y : ys)
+      | x == y = []
+      | otherwise = y : fLength x ys
+    fLength _ _ = error "fLength called on empty list"
 
--- | Rotate a list n places to the left, e.g. 
---   rotate 2 ['#','#','#','@'] becomes 
+-- | Rotate a list n places to the left, e.g.
+--   rotate 2 ['#','#','#','@'] becomes
 --   ['#','@','#','#'].
 rotate :: Int -> [a] -> [a]
 rotate n xs = bs ++ as where (as, bs) = splitAt (n `mod` length xs) xs
 
--- | Rotate a list n places to the right, e.g. 
---   rotateR 2 ['#','@','#','#'] becomes 
+-- | Rotate a list n places to the right, e.g.
+--   rotateR 2 ['#','@','#','#'] becomes
 --   ['#','#','#','@'].
 rotateR :: Int -> [a] -> [a]
 rotateR n xs = rotate (length xs - n) xs
 
-deleteAll :: Eq a => [a] -> [a] -> [a]
+deleteAll :: (Eq a) => [a] -> [a] -> [a]
 deleteAll ds ls = foldl (flip delete) ls ds
 
-uninterleave :: [a] -> ([a],[a])
-uninterleave [] = ([],[])
-uninterleave [a] = ([a],[])
-uninterleave (a:as) = first (a :) as' where
-  as' = uninterleave' as
-  uninterleave' :: [a] -> ([a],[a])
-  uninterleave' [] = ([],[])
-  uninterleave' [b] = ([],[b])
-  uninterleave' (b:bs) = second (b :) bs' where
-    bs' = uninterleave bs
+uninterleave :: [a] -> ([a], [a])
+uninterleave [] = ([], [])
+uninterleave [a] = ([a], [])
+uninterleave (a : as) = first (a :) as'
+  where
+    as' = uninterleave' as
+    uninterleave' :: [a] -> ([a], [a])
+    uninterleave' [] = ([], [])
+    uninterleave' [b] = ([], [b])
+    uninterleave' (b : bs) = second (b :) bs'
+      where
+        bs' = uninterleave bs
 
-splitOn :: Eq a => [a] -> [a] -> [[a]]
+splitOn :: (Eq a) => [a] -> [a] -> [[a]]
 splitOn _ [] = []
 splitOn split list = case indexOf split list of
   Nothing -> [list]
-  Just i  -> take i list : splitOn split (drop (i + length split) list)
+  Just i -> take i list : splitOn split (drop (i + length split) list)
 
 -- | Returns the first index of the needle if contained in the haystack
 --   and Nothing otherwise.
 --   e.g. indexOf "foo" "hafoobar" = 2.
-indexOf :: Eq a => [a] -> [a] -> Maybe Int
-indexOf = indexOf' 0 where
-    indexOf' :: Eq a => Int -> [a] -> [a] -> Maybe Int
+indexOf :: (Eq a) => [a] -> [a] -> Maybe Int
+indexOf = indexOf' 0
+  where
+    indexOf' :: (Eq a) => Int -> [a] -> [a] -> Maybe Int
     indexOf' _ _ [] = Nothing
-    indexOf' index needle haystack = if needle `isPrefixOf` haystack
-      then Just index
-      else indexOf' (index+1) needle (tail haystack)
+    indexOf' index needle haystack =
+      if needle `isPrefixOf` haystack
+        then Just index
+        else indexOf' (index + 1) needle (tail haystack)
 
 swapElems :: Int -> Int -> [a] -> [a]
 swapElems _ _ [] = []
 swapElems i j xs
   | i == j = xs
-  | i > j  = swapElems j i xs
-swapElems i j xs = let
-              elemI = xs !! (i `mod` length xs)
-              elemJ = xs !! (j `mod` length xs)
-              initial = take i xs
-              middle = take (j - i - 1) $ drop (i+1) xs
-              final = drop (j+1) xs
-                   in initial ++ [elemJ] ++ middle ++ [elemI] ++ final
+  | i > j = swapElems j i xs
+swapElems i j xs =
+  let elemI = xs !! (i `mod` length xs)
+      elemJ = xs !! (j `mod` length xs)
+      initial = take i xs
+      middle = take (j - i - 1) $ drop (i + 1) xs
+      final = drop (j + 1) xs
+   in initial ++ [elemJ] ++ middle ++ [elemI] ++ final
 
 moveElem :: Int -> Int -> [a] -> [a]
 moveElem i j s
   | i == j = s
-moveElem x y s = let
-        initial = take x s
-        elemX = s !! x
-        final = drop (x+1) s
-        s' = initial ++ final
-        initial' = take y s'
-        final' = drop y s'
-                     in initial' ++ [elemX] ++ final'
+moveElem x y s =
+  let initial = take x s
+      elemX = s !! x
+      final = drop (x + 1) s
+      s' = initial ++ final
+      initial' = take y s'
+      final' = drop y s'
+   in initial' ++ [elemX] ++ final'
 
 -- | Replaces the element at the given index of a list.
 replace :: Int -> a -> [a] -> [a]
 replace _ _ [] = error "replace: index out of bounds"
-replace n r (a:as)
-  | n < 0      = replace (negate n) a (reverse as)
-  | n == 0     = r:as
-  | otherwise  = a : replace (pred n) r as
+replace n r (a : as)
+  | n < 0 = replace (negate n) a (reverse as)
+  | n == 0 = r : as
+  | otherwise = a : replace (pred n) r as
 
 -- | Aggregates a list. If two successive elements are equal by the repr function, they are combined via the combine function.
 aggregateOn :: (Eq b) => (a -> b) -> (a -> a -> a) -> [a] -> [a]
 aggregateOn _ _ [] = []
 aggregateOn _ _ [a] = [a]
 aggregateOn repr combine (a : b : cs) =
-    if repr a == repr b
-        then aggregateOn repr combine (combine a b : cs)
-        else a : aggregateOn repr combine (b : cs)
+  if repr a == repr b
+    then aggregateOn repr combine (combine a b : cs)
+    else a : aggregateOn repr combine (b : cs)
 
-differences :: Num a => [a] -> [a]
+differences :: (Num a) => [a] -> [a]
 differences ns = zipWith (-) (drop 1 ns) ns
 
-monotone :: (Num a, Ord a) =>[a] -> Bool
+monotone :: (Num a, Ord a) => [a] -> Bool
 monotone ns = allDecreasing ns || allIncreasing ns
 
-allDecreasing :: (Num a, Ord a) =>[a] -> Bool
-allDecreasing ns = let
-    ds = zipWith (-) ns (drop 1 ns)
-    in all (<0) ds
+allDecreasing :: (Num a, Ord a) => [a] -> Bool
+allDecreasing ns =
+  let ds = zipWith (-) ns (drop 1 ns)
+   in all (< 0) ds
 
-allIncreasing :: (Num a, Ord a) =>[a] -> Bool
-allIncreasing ns = let
-    ds = zipWith (-) ns (drop 1 ns)
-    in all (>0) ds
+allIncreasing :: (Num a, Ord a) => [a] -> Bool
+allIncreasing ns =
+  let ds = zipWith (-) ns (drop 1 ns)
+   in all (> 0) ds
 
 subsets :: [Int] -> [[Int]]
-subsets []  = [[]]
-subsets (x:xs) = subsets xs ++ map (x:) (subsets xs)
+subsets [] = [[]]
+subsets (x : xs) = subsets xs ++ map (x :) (subsets xs)
 
 none :: (a -> Bool) -> [a] -> Bool
 none predicate = not . any predicate
 
 -- | sorted is true if every element in the list is less than or equal to every successive element
-sorted :: Ord a => [a] -> Bool
+sorted :: (Ord a) => [a] -> Bool
 sorted [] = True
 sorted [_] = True
-sorted (a:as) = all (a <=) as && sorted as
+sorted (a : as) = all (a <=) as && sorted as
+
+sublist :: Int -> Int -> [a] -> [a]
+sublist x y = take (y - x) . drop x
